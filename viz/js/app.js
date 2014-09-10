65afaf98d2edf66cf6fed81e40fff15fe0f148f9
Peter Desmet <peter.desmet.work@gmail.com>
2014-09-10 16:20:38 +0200

    Reorder functions

--- viz/js/app.js
+++ viz/js/app.js
@@ -32,7 +32,37 @@ function drawMap() {
     showBlackoutDataOnMap(window.map);
 };
 
-// show the detailed info of a municipality in the sidebar
+function showBlackoutDataOnMap(map) {
+    var sql = "WITH rolling_blackout_by_municipality AS (SELECT municipality, municipality_geojson, sum(coalesce(section_all,0)) AS section_all, sum(total) AS total, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_1,0))/sum(total)*100,2) ELSE 0 END AS section_1_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_2,0))/sum(total)*100,2) ELSE 0 END AS section_2_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_3,0))/sum(total)*100,2) ELSE 0 END AS section_3_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_4,0))/sum(total)*100,2) ELSE 0 END AS section_4_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_5,0))/sum(total)*100,2) ELSE 0 END AS section_5_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_6,0))/sum(total)*100,2) ELSE 0 END AS section_6_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_all,0))/sum(total)*100,2) ELSE 0 END AS section_all_pct FROM rolling_blackout GROUP BY municipality, municipality_geojson ) SELECT m.cartodb_id, m.the_geom, m.the_geom_webmercator, m.region, b.* FROM rolling_blackout_by_municipality b LEFT JOIN municipalities_belgium m ON b.municipality_geojson = m.name ORDER BY b.municipality";
+    var section = "section_all_pct"
+    var cartocss = "#rolling_blackout { polygon-fill: #1a9850; polygon-opacity: 0.8; line-color: #333333; line-width: 0.5; line-opacity: 1; } #rolling_blackout[" + section + " = 100] { polygon-fill: #d73027; } #rolling_blackout[" + section + " < 100] { polygon-fill: #f79272; } #rolling_blackout[" + section + " < 80] { polygon-fill: #fed6b0; } #rolling_blackout[" + section + " < 60] { polygon-fill: #fff2cc; } #rolling_blackout[" + section + " < 40] { polygon-fill: #d2ecb4; } #rolling_blackout[" + section + " < 20] { polygon-fill: #8cce8a; } #rolling_blackout[" + section + " = 0] { polygon-fill: #1a9850; }";
+    cartodb.createLayer(map, {
+        user_name: "datafable",
+        type: "cartodb",
+        sublayers: [{
+            sql: sql,
+            cartocss: cartocss
+        }]
+    })
+    .addTo(map)
+    .done(function(layer) {
+        window.mapLayer = layer;
+        var sublayer = layer.getSubLayer(0);
+        sublayer.setInteraction(true);
+        var selectedFields = [
+            "municipality",
+            "section_all",
+            "total",
+            "section_all_pct"
+        ];
+        sublayer.set({"interactivity": selectedFields});
+        sublayer.on("featureClick", function(event, latlng, pos, data, layerindex) {
+            // Show the detailed info of a municipality in the sidebar
+            showMunicipalityInfo(data);
+        });
+    });
+};
+
 function showMunicipalityInfo(data) {
     var sectionField = "section_" + selectedSection + "_pct";
     var sql = "SELECT district, section_1_pct, section_2_pct, section_3_pct, section_4_pct, section_5_pct, section_6_pct, section_all_pct FROM rolling_blackout WHERE municipality_geojson='" + data.municipality + "';";
@@ -77,36 +107,6 @@ function showMunicipalityInfo(data) {
         highlightSectionInTable();
     });
 }
-
-function showBlackoutDataOnMap(map) {
-    var sql = "WITH rolling_blackout_by_municipality AS (SELECT municipality, municipality_geojson, sum(coalesce(section_all,0)) AS section_all, sum(total) AS total, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_1,0))/sum(total)*100,2) ELSE 0 END AS section_1_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_2,0))/sum(total)*100,2) ELSE 0 END AS section_2_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_3,0))/sum(total)*100,2) ELSE 0 END AS section_3_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_4,0))/sum(total)*100,2) ELSE 0 END AS section_4_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_5,0))/sum(total)*100,2) ELSE 0 END AS section_5_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_6,0))/sum(total)*100,2) ELSE 0 END AS section_6_pct, CASE WHEN sum(total) != 0 THEN round(sum(coalesce(section_all,0))/sum(total)*100,2) ELSE 0 END AS section_all_pct FROM rolling_blackout GROUP BY municipality, municipality_geojson ) SELECT m.cartodb_id, m.the_geom, m.the_geom_webmercator, m.region, b.* FROM rolling_blackout_by_municipality b LEFT JOIN municipalities_belgium m ON b.municipality_geojson = m.name ORDER BY b.municipality";
-    var section = "section_all_pct"
-    var cartocss = "#rolling_blackout { polygon-fill: #1a9850; polygon-opacity: 0.8; line-color: #333333; line-width: 0.5; line-opacity: 1; } #rolling_blackout[" + section + " = 100] { polygon-fill: #d73027; } #rolling_blackout[" + section + " < 100] { polygon-fill: #f79272; } #rolling_blackout[" + section + " < 80] { polygon-fill: #fed6b0; } #rolling_blackout[" + section + " < 60] { polygon-fill: #fff2cc; } #rolling_blackout[" + section + " < 40] { polygon-fill: #d2ecb4; } #rolling_blackout[" + section + " < 20] { polygon-fill: #8cce8a; } #rolling_blackout[" + section + " = 0] { polygon-fill: #1a9850; }";
-    cartodb.createLayer(map, {
-        user_name: "datafable",
-        type: "cartodb",
-        sublayers: [{
-            sql: sql,
-            cartocss: cartocss
-        }]
-    })
-    .addTo(map)
-    .done(function(layer) {
-        window.mapLayer = layer;
-        var sublayer = layer.getSubLayer(0);
-        sublayer.setInteraction(true);
-        var selectedFields = [
-            "municipality",
-            "section_all",
-            "total",
-            "section_all_pct"
-        ];
-        sublayer.set({"interactivity": selectedFields});
-        sublayer.on("featureClick", function(event, latlng, pos, data, layerindex) {
-            showMunicipalityInfo(data);
-        });
-    });
-};
 
 function changeSectionOnMap() {
     var section = "section_" + window.selectedSection + "_pct";
